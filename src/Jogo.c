59d1d3823e23bdbe01504bb445c9c9086c619703
brenopinna <brenopinna1008@gmail.com>
2024-11-27 20:03:07 -0300

    :sparkles: feat: adiciona configuracao inicial para uso do teclado

--- src/Jogo.c
+++ src/Jogo.c
@@ -9,23 +9,26 @@ struct Jogo {
   ALLEGRO_TIMER *timer;
   ALLEGRO_EVENT_QUEUE *queue;
   // ALLEGRO_FONT *font;
+  bool *keys;
   ALLEGRO_EVENT event;
 };
 
 Jogo *novo_jogo() {
   al_init();
   // al_init_font_addon();
+  al_install_keyboard();
 
   Jogo *J = malloc(sizeof(Jogo));
 
   J->disp = al_create_display(600, 400);
-  J->timer = al_create_timer(1.0 / 100.0);
+  J->timer = al_create_timer(1.0 / 10.0);
   J->queue = al_create_event_queue();
+  J->keys = calloc(ALLEGRO_KEY_MAX, sizeof(bool));
   // J->font = al_create_builtin_font();
 
+  al_register_event_source(J->queue, al_get_keyboard_event_source());
   al_register_event_source(J->queue, al_get_display_event_source(J->disp));
   al_register_event_source(J->queue, al_get_timer_event_source(J->timer));
-
   al_start_timer(J->timer);
 
   return J;
@@ -39,15 +42,24 @@ bool jogo_rodando(Jogo *J) {
 }
 
 void atualizar_jogo(Jogo *J) {
-  bool redraw = false;
+  bool redraw = true;
 
   al_wait_for_event(J->queue, &J->event);
 
-  if (J->event.type == ALLEGRO_EVENT_TIMER) {
+  ALLEGRO_EVENT event = J->event;
+
+  if (event.type == ALLEGRO_EVENT_TIMER) {
     redraw = true;
+  } else if (event.type == ALLEGRO_EVENT_KEY_DOWN) {
+    J->keys[event.keyboard.keycode] = true;
+  } else if (event.type == ALLEGRO_EVENT_KEY_UP) {
+    J->keys[event.keyboard.keycode] = false;
   }
 
   if (redraw && al_is_event_queue_empty(J->queue)) {
+    if (J->keys[ALLEGRO_KEY_DOWN] || J->keys[ALLEGRO_KEY_UP]) {
+      // fazer alguma coisa
+    }
     al_clear_to_color(al_map_rgb(150, 150, 200));
     // al_draw_text(font, al_map_rgb(255, 255, 255), 300, 200, ALLEGRO_ALIGN_CENTRE, "Alô mundo!");
     al_flip_display();
@@ -61,5 +73,6 @@ void finalizar_jogo(Jogo *J) {
   al_destroy_display(J->disp);
   al_destroy_timer(J->timer);
   al_destroy_event_queue(J->queue);
+  free(J->keys);
   free(J);
 }
