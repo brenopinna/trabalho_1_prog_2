b6f3d4e3002bfda678e8f651a1b52b6e0186e2f7
JayAsPro <joaov.aproenca@gmail.com>
2025-01-23 11:27:21 -0300

    :ok_hand: style: mudanca de formatacao

--- src/ListaEncadeada.c
+++ src/ListaEncadeada.c
@@ -4,18 +4,18 @@
 #include <Mapa.h>
 #include <ListaEncadeada.h>
 
-MapNode* adicionar_mapa(const char* arquivo_mapa, MapNode* head){
-    MapNode* node = malloc(sizeof(MapNode));
-    strcpy(node->arquivo_mapa, arquivo_mapa);
-    node->prev = head;
-    node->next = NULL;
-    if(head != NULL) head->next = node;
-    return node;
+MapNode* adicionar_mapa(const char* arquivo_mapa, MapNode* head) {
+  MapNode* node = malloc(sizeof(MapNode));
+  strcpy(node->arquivo_mapa, arquivo_mapa);
+  node->prev = head;
+  node->next = NULL;
+  if (head != NULL) head->next = node;
+  return node;
 }
 
-void remover_mapas(MapNode* head){
-    if(head != NULL) {
-        remover_mapas(head->next);
-        free(head);
-    }
+void remover_mapas(MapNode* head) {
+  if (head != NULL) {
+    remover_mapas(head->next);
+    free(head);
+  }
 }
